1563d1da72dd97ecab7d7b73c4a4fcbe4bc2a32f
office <davelynch45@gmail.com>
2016-08-31 21:58:53 +0100

    test change only

--- TheWorld2/src/TheWorld2/Startup.cs
+++ TheWorld2/src/TheWorld2/Startup.cs
@@ -18,7 +18,7 @@ namespace TheWorld2
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to testconfigure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
